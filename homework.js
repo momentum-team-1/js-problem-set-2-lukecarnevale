12818b59979897b6de3e6df7fcb75099a9e75903
lukecarnevale <lukecarnevale19@gmail.com>
2020-04-28 22:03:56 -0400

    this one hurt

--- homework.js
+++ homework.js
@@ -6,30 +6,66 @@
 // If the potential member is not in the array, return the array unchanged.
 // If the potential member is in the array, remove all instances of it from the array.
 
+function remove(array,member){
+    let newArray = [];
+    for (let name of array){
+        if (name !== member){
+            newArray.push(name);
+        }
+    }
+    return newArray;
+}
+
 // 2. Revisit your "remove" function. Make sure that it does not change the original
 // array but instead returns a new array.
 
 // 3. Create a function called "sum" that takes an array of numbers and
 // returns the sum of those numbers.
+function sum(array){
+    let total = 0;
+    for (let item of array){
+        total += item;
+    }
+    return total;
+}
 
 // 4. Create a function called "average" that takes an array of numbers
 // and returns the average of those numbers.
+function average(array) {
+    if (array.length === 0) {
+      return;
+    } else {
+      let total = 0;
+      for (let item of array) {
+        total += item;
+      }
+      return total / array.length;
+    }
+  }
 
-// 5. Create a function called "minimum" that takes an array of numbers and
-// returns the smallest number in that array.
+// Not sure why this isnt working...
+//function getAverage(arr){
+//     var arr = 0;    
 
-// 6. There are many techniques to sort arrays in programming. Your programming
-// language will likely include the ability to do this. We are going to
-// implement sorting ourselves, however.
-//
-// A "selection sort" is one of the most simple sorting algorithms. To implement it,
-// you start with an unsorted array of numbers. You search the array and find the
-// smallest number in the array. You then insert that into a new array as the first
-// element and remove it from the original array. You continue doing this until
-// there are no numbers left in the original array.
-//
-// Create a function called selectionSort that takes an array of numbers and returns
-// a sorted array using the above technique.
+//     for (var i=0;i < arr.length; i++){
+//         sum += arr[i];
+//     }
+//     return sum/arr.length;
+// }
+
+
+// 5. Create a function called "minimum" that takes an array of numbers and returns the smallest number in that array.
+function minimum(array){
+    if (array.length === 0){
+        return;
+    } else {
+        return Math.min(...array);
+    }
+}
+
+// 6. There are many techniques to sort arrays in programming. Your programming language will likely include the ability to do this. We are going to implement sorting ourselves, however.
+// A "selection sort" is one of the most simple sorting algorithms. To implement it, you start with an unsorted array of numbers. You search the array and find the smallest number in the array. You then insert that into a new array as the first element and remove it from the original array. You continue doing this until there are no numbers left in the original array.
+// Create a function called selectionSort that takes an array of numbers and returns a sorted array using the above technique.
 //
 // Note 1: You do not actually want to delete things from the original array. You
 // should copy it first. To copy an array, use the following code:
@@ -42,8 +78,16 @@
 // https://courses.cs.vt.edu/csonline/Algorithms/Lessons/SelectionSort/index.html
 // to see how. This may make more sense to you.
 
+function selectionSort(array){
+    var arrayCopy = array.slice(0);
+    return arrayCopy.sort((a,b)=> a-b)
+}
+
+
 // 7. Create a function called `textList` that takes an array and joins its elements
 // into a string separated by commas.
 //
 // For example, `textList(['Cadence', 'Ordel', 'Marion'])` results in the string
 // `"Cadence,Ordel,Marion"`.
+
+
